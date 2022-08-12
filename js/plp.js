20b41b702539e815fa32c9bcc2b19b62e1ab4f62
yakhyo04 <fenixrelaxation@gmail.com>
2022-08-12 15:53:24 +0500

    happened some changes

--- js/plp.js
+++ js/plp.js
@@ -1,26 +1,94 @@
-const DATA = [
-    {
-        id: 1,
-        title: 'Variety Pack',
-        desc: '- Delicate and lean to sip',
-        content: 'Adaptogenic ingredients:  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-        content2: 'Adaptogenic ingredients:  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-    },
-    {
-        id: 2,
-        image: '../media/png/funkin.png',
-        title: 'Bottle',
-        reviews: '../media/svg/black-star.svg',
-        num: '(32 reviews)',
-        price: '$35.59',
-        cta: 'Shop Now'
-    },
-    {
-        id: 3,
-        image: '../media/png/funkin.png',
-        title: 'Can',
-        reviews: '../media/svg/white-star.svg',
-        price: '$15.59',
-        cta: 'Shop Now'
-    }
-]
+// const shopItem = document.querySelector('.shop__list');
+
+// const DATA = [
+//     {
+//         id: 1,
+//         image: '../media/png/funkin.png',
+//         title: 'Bottle',
+//         reviews: '../media/svg/black-star.svg',
+//         num: '(32 reviews)',
+//         price: '$35.59',
+//         cta: 'Shop Now'
+//     },
+//     {
+//         id: 2,
+//         image: '../media/png/funkin.png',
+//         title: 'Bottle',
+//         reviews: '../media/svg/black-star.svg',
+//         num: '(32 reviews)',
+//         price: '$35.59',
+//         cta: 'Shop Now'
+//     },
+//     {
+//         id: 3,
+//         image: '../media/png/funkin.png',
+//         title: 'Bottle',
+//         reviews: '../media/svg/black-star.svg',
+//         num: '(32 reviews)',
+//         price: '$35.59',
+//         cta: 'Shop Now'
+//     },
+//     {
+//         id: 4,
+//         image: '../media/png/funkin.png',
+//         title: 'Bottle',
+//         reviews: '../media/svg/black-star.svg',
+//         num: '(32 reviews)',
+//         price: '$35.59',
+//         cta: 'Shop Now'
+//     },
+//     {
+//         id: 5,
+//         image: '../media/png/funkin.png',
+//         title: 'Bottle',
+//         reviews: '../media/svg/black-star.svg',
+//         num: '(32 reviews)',
+//         price: '$35.59',
+//         cta: 'Shop Now'
+//     },
+// ]
+
+// let num = 5;
+// let product = '';
+
+// console.log(DATA)
+
+// function renderProductsToDOM(){
+//     DATA.map(({id, image, title, reviews, num, price, cta}) => {
+//         product = `
+//         <div class="shop__item">
+//             <div key=${id} class="shop__item--wrapper">
+//         <img class="shop__image" src=${image} alt="funkin">
+//         <div class="shop__item--box">
+//         <h3 class="shop__subtitle">${title}</h3>
+//         <ul class="shop__stars">
+//         <li class="shop__star">
+//             <img class="shop__star--image" src=${reviews} alt="black">
+//         </li>
+//         <li class="shop__star">
+//             <img class="shop__star--image" src=${reviews} alt="black">
+//         </li>
+//         <li class="shop__star">
+//             <img class="shop__star--image" src=${reviews} alt="black">
+//         </li>
+//         <li class="shop__star">
+//             <img class="shop__star--image" src=${reviews} alt="black">
+//         </li>
+//         <li class="shop__star">
+//             <img class="shop__star--image" src=${reviews} alt="black">
+//         </li>
+//         <li>
+//             ${num}
+//         </li>
+//         </ul>
+//         <p class="shop__item--price">${price}</p>
+//         <a class="shop__item--cta" href="#">${cta}</a>
+//         </div>
+//     </div>
+//         </div>
+//         ` 
+//     })
+//     shopItem.append(product);
+// }
+
+// renderProductsToDOM();
